3ffd2dd4805e0f386131fcd24508a478dad83558
Kate <lazar.k@wp.pl>
2023-06-17 17:50:12 +0200

    generic method swap

--- AdvancedJava/src/main/java/SDA/genericTypes/Main.java
+++ AdvancedJava/src/main/java/SDA/genericTypes/Main.java
@@ -1,5 +1,8 @@
 package SDA.genericTypes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -8,5 +11,31 @@ public class Main {
         Pair<String, String> pair3 = new Pair<>("kolejna para", "trzecia para");
 
         System.out.println(pair1.getFirst() + "  " + pair1.getSecond());
+        System.out.println();
+
+        Integer[] table = new Integer[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.print("Table: ");
+        printTable(table);
+        swap(table, 4, 8);
+        System.out.print("Table after swap: ");
+        printTable(table);
+    }
+
+    // Napisz metodę generyczną, które zadaniem będzie zamiana (swap) elementów o wskazanych pozycjach w tablicy.
+    // Metoda powinna przyjmować:
+    // - tablicę dowolnego typu elementów
+    // - pozycję pierwszego elementu
+    // - pozycję drugiego elementu
+    // tzn, np. jeśli mamy tablice 10 elementów, podamy pozycję 1: 4, pozycję 2: 8 to należy dokonać zamiany:
+    // - element pod indeksem 4 musi znaleść się pod indeksem 8, a element o indeksie 8 musi znaleść się pod indeksem 4
+    public static <E> void swap(E[] tab, int pos1, int pos2) {
+        E temp = tab[pos1];
+        tab[pos1] = tab[pos2];
+        tab[pos2] = temp;
+    }
+
+    public static <E> void printTable(E[] tab) {
+        for (E item : tab) System.out.print(item + ", ");
+        System.out.println();
     }
 }
