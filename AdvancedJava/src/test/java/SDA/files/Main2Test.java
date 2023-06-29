0ee010f5ca1b439090f08800c13107db769f9825
Kate <lazar.k@wp.pl>
2023-06-29 21:45:44 +0200

    creating Files and writing to them

--- AdvancedJava/src/test/java/SDA/files/Main2Test.java
+++ AdvancedJava/src/test/java/SDA/files/Main2Test.java
@@ -8,7 +8,6 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class Main2Test {
     @Test
